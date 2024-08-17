6adf30884e2046c252a1b1f8c0602a0ea97e3977
saba-ramezani <saba.rad.sr@gmail.com>
2024-08-17 17:47:45 +0330

    added showing different job positions in banner with animation

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,6 +4,47 @@ import headerImg from "../assets/img/header-img.svg";
 import { useEffect, useState } from "react";
 
 function Banner() {
+    const [loopNum, setLoopNum] = useState(0);
+    const [isDeleting, setIsDeleting] = useState(false);
+    const [text, setText] = useState('');
+    const [delta, setDelta] = useState(500);
+    const [index, setIndex] = useState(1);
+    const toRotate = [ "Web Developer", "React Native Developer"];
+    const period = 2000;
+
+    useEffect(() => {
+        let ticker = setInterval(() => {
+            tick();
+        }, delta);
+
+        return () => { clearInterval(ticker) };
+    }, [text]);
+
+    const tick = () => {
+        let i = loopNum % toRotate.length;
+        let fullText = toRotate[i];
+        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    
+        setText(updatedText);
+    
+        if (isDeleting) {
+          setDelta(prevDelta => prevDelta / 2);
+        }
+    
+        if (!isDeleting && updatedText === fullText) {
+          setIsDeleting(true);
+          setIndex(prevIndex => prevIndex - 1);
+          setDelta(period);
+        } else if (isDeleting && updatedText === '') {
+          setIsDeleting(false);
+          setLoopNum(loopNum + 1);
+          setIndex(1);
+          setDelta(500);
+        } else {
+          setIndex(prevIndex => prevIndex + 1);
+        }
+      }
+    
 
     return (
         <section className="banner" id="home">
@@ -12,8 +53,9 @@ function Banner() {
                     <Col xs={12} md={6} xl={7}>
                         <span className="tagline">Welcome to my Portfolio</span>
                         <h1>
-                            {'Hi I\'m webdecoded'}
-                            <span className="wrap">web developer</span>
+                            {'Hi, I\'m Saba\n'}
+                            <br />
+                            <span className="wrap">{text}</span>
                         </h1>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis fuga ad iusto cupiditate impedit eos porro officiis a dicta optio? Nulla, iusto delectus sunt et nesciunt quo ut harum optio!</p>
                         <button onClick={() => console.log('connect btn clicked')}>Let's connect</button>
